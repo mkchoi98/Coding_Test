93c3149ce0e0ae84b7a64fb5a621c1957df6af6e
MinKyeong Choi <59993071+Choi-Minkyeong@users.noreply.github.com>
2023-05-10 00:07:17 +0900

    Create 콜라 문제.c

---
+++ Programmers/콜라 문제.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    while (n >= a)
+    {
+        answer += (n/a)*b;
+      
+        n = (n%a) + (n/a)*b;
+    }
+    
+    return answer;
+}
