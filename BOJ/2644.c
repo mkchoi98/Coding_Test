2f4bc0d4f1e6f9168252d6478a7dbed52bce6186
MinKyeong Choi <59993071+Choi-Minkyeong@users.noreply.github.com>
2023-05-24 13:43:41 +0900

    solve

--- BOJ/2644.c
+++ BOJ/2644.c
@@ -1,71 +1,60 @@
-//
-//  main.c
-//  2644
-//
-//  Created by 최민경 on 2023/05/23.
-//
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
-void DFS(int adj[][101], int start, int end, int visited[], int n, int *flag)
+void DFS(int adj[][101], int visited[], int now, int end, int *cnt, int n, int *answer)
 {
-    int i;
-    
-    if (start == end)
-    {
-        *flag = 1;
-        return;
-    }
-    
-    visited[start] = 1;
-    //printf("%d ", start);
-    
-    for (i=1;i<=n;i++)
-    {
-        if (adj[start][i] == 1 && visited[i] == 0 && !(*flag))
-        {
-            //printf("%d\n", i);
-            DFS(adj, i, end, visited, n, flag);
-        }
-    }
+	int i;
+
+	if (now == end)
+	{
+		*answer = *cnt;
+		return;
+	}
+
+	visited[now] = 1;
+
+	for (i = 1; i <= n; i++)
+	{
+		if (adj[now][i] == 1 && visited[i] != 1)
+		{
+			(*cnt)++;
+			
+			DFS(adj, visited, i, end, cnt, n, answer);
+		}
+	}
+	(*cnt)--;
 }
 
-int main(int argc, const char * argv[]) {
-    int n, m;
-    int a, b;
-    int x, y;
-    int i;
-    int adj[101][101] = {0, }, visited[101] = {0};
-    int cnt = 0;
-    int flag = 0;
-    
-    scanf("%d", &n);
-    scanf("%d %d", &a, &b);
-    scanf("%d", &m);
-    
-    for (i=0;i<m;i++)
-    {
-        scanf("%d %d", &x, &y);
-        adj[x][y] = 1;
-        adj[y][x] = 1;
-    }
-    
-    if (a > b)
-        DFS(adj, a, b, visited, n, &flag);
-    else
-        DFS(adj, b, a, visited, n, &flag);
-    
-    if (flag)
-    {
-        for (i=1;i<=n;i++)
-        {
-            if (visited[i] != 0) cnt++;
-        }
-        
-        printf("%d\n", cnt);
-    }
-    
-    else printf("-1\n");
-    
-    return 0;
+int main()
+{
+	int n;
+	int num1, num2;
+	int m;
+	int x, y;
+	int i;
+
+	int adj[101][101] = {0, };
+	int visited[101] = {0};
+
+	int cnt = 0;
+	int answer = 0;
+
+	scanf("%d", &n);
+	scanf("%d %d", &num1, &num2);
+	scanf("%d", &m);
+
+	for (i = 0; i < m; i++)
+	{
+		scanf("%d %d", &x, &y);
+		adj[x][y] = 1;
+		adj[y][x] = 1;
+	}
+
+	DFS(adj, visited, num1, num2, &cnt, n, &answer);
+
+	if (cnt != -1) printf("%d", answer);
+	else printf("-1");
+
+	return 0;
 }
