25266a70c7784a2434e9cb89159a45ad623b44b4
MinKyeong Choi <59993071+Choi-Minkyeong@users.noreply.github.com>
2023-06-13 11:33:26 +0900

    solve

--- BOJ/1389.c
+++ BOJ/1389.c
@@ -1,101 +1,99 @@
-#define _CRT_SECURE_NO_WARNINGS
+//
+//  main.c
+//  1389
+//
+//  Created by 최민경 on 2023/06/13.
+//
 
 #include <stdio.h>
 
 typedef struct Queue
 {
-	int data[101];
-	int front, rear;
+    int front, rear;
+    int data[10000];
 }Queue;
 
-void init(int* arr, int n)
+void init(int *arr, int n)
 {
-	int i;
-
-	for (i = 0; i <= n; i++)
-		arr[i] = 0;
+    int i;
+    
+    for (i=0;i<=n;i++)
+        arr[i] = 0;
 }
 
-void BFS(int adj[][101], int discovered[101], int start, int end, int n, int *cnt)
+void BFS(int adj[][101], int discovered[], int n, int start)
 {
-	Queue que;
-	int i, index;
-
-	que.front = que.rear = -1;
-
-	discovered[start] = 1;
-
-	que.data[++que.rear] = start;
-
-	while (que.front < que.rear)
-	{
-		index = que.data[++que.front];
-		
-		//if (adj[index][end])
-			printf("%d ", index);
-
-		for (i = 1; i <= n; i++)
-		{
-			if (adj[index][i] == 1 && discovered[i] != 1)
-			{
-				if (i == end) return;
-
-				discovered[i] = 1;
-				que.data[++que.rear] = i;
-			}
-		}
-	}
+    Queue que;
+    int data;
+    int i;
+    
+    que.rear = que.front = -1;
+    
+    que.data[++que.rear] = start;
+    discovered[start] = 1;
+    
+    while (que.front < que.rear)
+    {
+        data = que.data[++que.front];
+        
+        //printf("%d ", data);
+        for (i=1;i<=n;i++)
+        {
+            if (adj[data][i] == 1 && discovered[i] == 0)
+            {
+                que.data[++que.rear] = i;
+                discovered[i] = discovered[data]+1;
+                
+                //printf("%d ", i);
+            }
+        }
+    }
 }
 
-int main()
-{
-	int n, m;
-	int x, y;
+int main(int argc, const char * argv[]) {
+    int n, m;
+    int a, b;
+    int adj[101][101] = {0,}, discovered[101] = {0};
+    int i, j;
+    int sum[101] = {0};
+    int min, min_index;
+    
+    scanf("%d %d", &n, &m);
+    
+    for (i=0;i<m;i++)
+    {
+        scanf("%d %d", &a, &b);
+        
+        adj[a][b] = 1;
+        adj[b][a] = 1;
+    }
+    
+    for (i=1;i<=n;i++)
+    {
+        init(discovered, n);
+        
+        BFS(adj, discovered, n, i);
+        
+        for (j=1;j<=n;j++)
+        {
+            sum[i] += discovered[j];
+        }
+        //printf("\n");
+    }
+    
+    min = sum[1];
+    min_index = 1;
+    
+    for (i=2;i<=n;i++)
+    {
+        if (min > sum[i])
+        {
+            min = sum[i];
+            min_index = i;
+        }
+    }
+    
+    printf("%d", min_index);
 
-	int adj[101][101] = { 0, };
-	int discovered[101] = { 0 };
-
-	int i, j;
-
-	int min = 5000, cnt = 0, sub_cnt = 0, sum = 0;
-	int answer;
-
-	scanf("%d %d", &n, &m);
-
-	for (i = 0; i < m; i++)
-	{
-		scanf("%d %d", &x, &y);
-		adj[x][y] = 1;
-		adj[y][x] = 1;
-	}
-
-	for (i = 1; i <= n; i++)
-	{
-		sum = 0;
-		cnt = 0;
-		sub_cnt = 0;
-		
-		for (j = 1; j <= n; j++)
-		{
-			if (i == j) continue;
-
-			printf("strat: %d end: %d\n", i, j);
-
-			init(discovered, n);
-
-			BFS(adj, discovered, i, j, n, &cnt);
-			
-			printf("result : %d\n", cnt);
-		}
-		printf("\n");
-		if (min > cnt)
-		{
-			min = cnt;
-			answer = i;
-		}
-	}
-
-	printf("%d", answer);
-
-	return 0;
+    return 0;
 }
