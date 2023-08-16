99b9f4ec78a2dbcebbcace2881cb840032c58112
MinKyeong Choi <59993071+Choi-Minkyeong@users.noreply.github.com>
2023-08-16 17:35:52 +0900

    230816

--- BOJ/2178.c
+++ BOJ/2178.c
@@ -1,89 +1,70 @@
-//
-//  main.c
-//  2178
-//
-//  Created by 최민경 on 2023/06/12.
-//
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
 typedef struct Queue
 {
-    int front, rear;
-    int x[10000], y[10000];
+	int front, rear;
+	int x[10000], y[10000];
 }Queue;
 
-void BFS(int adj[][101], int start_x, int start_y, int n, int m, int *cnt)
+void BFS(long adj[][101], int n, int m, int start_x, int start_y)
 {
-    Queue que;
-    int x, y;
+	Queue que;
+	int x, y;
 
-    que.rear = que.front = -1;
-    
-    que.x[++que.rear] = start_x;
-    que.y[que.rear] = start_y;
-    
-    adj[start_x][start_y] = 0;
-    
-    while (que.front < que.rear)
-    {
-        x = que.x[++que.front];
-        y = que.y[que.front];
-        
-        if (adj[x-1][y] == 1 && x > 0)
-        {
-            que.x[++que.rear] = x-1;
-            que.y[que.rear] = y;
-            
-            adj[x-1][y] = adj[x][y] + 1;
-        }
-        
-        if (adj[x+1][y] == 1 && x < n-1)
-        {
-            que.x[++que.rear] = x+1;
-            que.y[que.rear] = y;
-            
-            adj[x+1][y] = adj[x][y] + 1;
-        }
-        
-        if (adj[x][y-1] == 1 && y > 0)
-        {
-            que.x[++que.rear] = x;
-            que.y[que.rear] = y-1;
-            
-            adj[x][y-1] = adj[x][y] + 1;
-        }
-        
-        if (adj[x][y+1] == 1 && y < m-1)
-        {
-            que.x[++que.rear] = x;
-            que.y[que.rear] = y+1;
-            
-            adj[x][y+1]= adj[x][y] + 1;
-        }
-        
-    }
+	int x_dir[4] = { 1, -1, 0, 0 };
+	int y_dir[4] = { 0, 0, 1, -1 };
+	int cur_x, cur_y;
+
+	int i;
+
+	que.front = que.rear = -1;
+
+	que.x[++que.rear] = start_x;
+	que.y[que.rear] = start_y;
+
+	while (que.front < que.rear)
+	{
+		x = que.x[++que.front];
+		y = que.y[que.front];
+
+		for (i = 0; i < 4; i++)
+		{
+			cur_x = x + x_dir[i];
+			cur_y = y + y_dir[i];
+			if (cur_x < n && cur_x >= 0 && cur_y < m && cur_y >= 0 && adj[cur_x][cur_y] == 1)
+			{
+				que.x[++que.rear] = cur_x;
+				que.y[que.rear] = cur_y;
+
+				adj[cur_x][cur_y] = adj[x][y] + 1;
+			}
+		}
+	}
 }
 
-int main(int argc, const char * argv[]) {
-    int n, m;
-    int adj[101][101];
-    int i, j;
-    int cnt = 1;
-    
-    scanf("%d %d", &n, &m);
-    
-    for (i=0;i<n;i++)
-    {
-        for (j=0;j<m;j++)
-        {
-            scanf("%1d", &adj[i][j]);
-        }
-    }
-    
-    BFS(adj, 0, 0, n, m, &cnt);
-    
-    printf("%d", adj[n-1][m-1]+1);
-    
-    return 0;
+int main()
+{
+	int n, m;
+
+	int i, j;
+
+	long adj[101][101] = { 0, };
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			scanf("%1ld", &adj[i][j]);
+		}
+	}
+
+	BFS(adj, n, m, 0, 0);
+
+	printf("%ld", adj[n - 1][m - 1]);
+
+	return 0;
 }
