a4f1165f81700177f8f55d6f36e37ab332c9094c
MinKyeong Choi <59993071+Choi-Minkyeong@users.noreply.github.com>
2023-08-16 16:17:59 +0900

    230816

--- BOJ/2606.c
+++ BOJ/2606.c
@@ -1,50 +1,45 @@
-//
-//  main.c
-//  2606
-//
-//  Created by 최민경 on 2023/06/12.
-//
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
-void DFS(int adj[][101], int *visited, int start, int n, int *cnt)
+void DFS(int adj[][101], int* visited, int n, int start, int *cnt)
 {
-    int i;
-    
-    visited[start] = 1;
-    
-    for (i=1;i<=n;i++)
-    {
-        if (adj[start][i] == 1 && visited[i] == 0)
-        {
-            (*cnt)++;
-            //printf("%d ", i);
-            DFS(adj, visited, i, n, cnt);
-        }
-    }
+	int i;
+
+	visited[start] = 1;
+	//printf("%d ", start);
+	(*cnt)++;
+	for (i = 1; i <= n; i++)
+	{
+		if (visited[i] == 0 && adj[start][i] == 1)
+		{
+			DFS(adj, visited, n, i, cnt);
+		}
+	}
 }
 
-int main(int argc, const char * argv[]) {
-    int n, m;
-    int i;
-    int visited[101] = {0}, adj[101][101] ={0,};
-    int u, v;
-    int cnt = 0;
-    
-    scanf("%d", &n);
-    scanf("%d", &m);
-    
-    for (i=0;i<m;i++)
-    {
-        scanf("%d %d", &u, &v);
-        
-        adj[u][v] = 1;
-        adj[v][u] = 1;
-    }
-    
-    DFS(adj, visited, 1, n, &cnt);
-    
-    printf("%d", cnt);
-    
-    return 0;
+int main()
+{
+	int n, m;
+	int i;
+
+	int adj[101][101] = { 0, };
+	int x, y;
+	int visited[101] = { 0, };
+
+	int cnt = 0;
+	
+	scanf("%d %d", &n, &m);
+
+	for (i = 0; i < m; i++)
+	{
+		scanf("%d %d", &x, &y);
+
+		adj[x][y] = 1;
+		adj[y][x] = 1;
+	}
+
+	DFS(adj, visited, n, 1, &cnt);
+
+	printf("%d", cnt-1);
 }
