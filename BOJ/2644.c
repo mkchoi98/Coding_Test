90cac06357883622e00cc34b668ba4bf995e1184
MinKyeong Choi <59993071+Choi-Minkyeong@users.noreply.github.com>
2023-08-16 17:02:56 +0900

    230816

--- BOJ/2644.c
+++ BOJ/2644.c
@@ -2,7 +2,8 @@
 
 #include <stdio.h>
 
-void DFS(int adj[][101], int visited[], int now, int end, int *cnt, int n, int *answer)
+// DFS ver
+/*void DFS(int adj[][101], int visited[], int now, int end, int *cnt, int n, int *answer)
 {
 	int i;
 
@@ -55,6 +56,75 @@ int main()
 
 	if (cnt != -1) printf("%d", answer);
 	else printf("-1");
+
+	return 0;
+}*/
+
+// BFS ver
+typedef struct Queue
+{
+	int front, rear;
+	int data[101];
+}Queue;
+
+void BFS(int adj[][101], int *discovered, int n, int start, int end)
+{
+	Queue que;
+	int index;
+	
+	int i;
+
+	que.front = que.rear = -1;
+
+	discovered[start] = 1;
+	que.data[++que.rear] = start;
+
+	while (que.front < que.rear)
+	{
+		index = que.data[++que.front];
+
+		if (index == end) break;
+
+		for (i = 1; i <= n; i++)
+		{
+			if (discovered[i] == 0 && adj[index][i] != 0)
+			{
+				que.data[++que.rear] = i;
+				discovered[i] = 1;
+
+				discovered[i] = discovered[index] + 1;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	int a, b;
+	int m;
+
+	int i, j;
+	
+	int adj[101][101] = {0,};
+	int discovered[101] = {0,};
+	int x, y;
+
+	scanf("%d", &n);
+	scanf("%d %d", &a, &b);
+	scanf("%d", &m);
+
+	for (i = 0; i < m; i++)
+	{
+		scanf("%d %d", &x, &y);
+
+		adj[x][y] = 1;
+		adj[y][x] = 1;
+	}
+
+	BFS(adj, discovered, n, a, b);
+
+	printf("%d", discovered[b] -1);
 
 	return 0;
 }
