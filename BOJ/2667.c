06790c175a6d56ac3c05f305ee83a371b8f8614c
MinKyeong Choi <59993071+Choi-Minkyeong@users.noreply.github.com>
2023-08-17 15:53:35 +0900

    230817

--- BOJ/2667.c
+++ BOJ/2667.c
@@ -5,7 +5,8 @@
 //  Created by 최민경 on 2023/06/12.
 //
 
-#include <stdio.h>
+//DFS
+/*#include <stdio.h>
 #include <stdlib.h>
 
 void DFS(int adj[][25], int n, int row, int col, int *arr, int cnt)
@@ -86,4 +87,128 @@ int main(int argc, const char * argv[]) {
     }
     
     return 0;
+}*/
+
+// BFS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+typedef struct Queue
+{
+	int front, rear;
+	int x[626], y[626];
+}Queue;
+
+void BFS(int adj[][26], int n, int start_x, int start_y, int num)
+{
+	Queue que;
+
+	int x, y;
+	int next_x, next_y;
+	int dir_x[4] = { 1, -1, 0, 0 };
+	int dir_y[4] = { 0, 0, 1, -1 };
+
+	int i, j;
+
+	que.front = que.rear = -1;
+
+	que.x[++que.rear] = start_x;
+	que.y[que.rear] = start_y;
+
+	adj[start_x][start_y] = num;
+
+	while (que.front < que.rear)
+	{
+		x = que.x[++que.front];
+		y = que.y[que.front];
+
+		for (i = 0; i < 4; i++)
+		{
+			next_x = x + dir_x[i];
+			next_y = y + dir_y[i];
+
+			if (next_x >= 0 && next_x < n && next_y >= 0 && next_y < n && adj[next_x][next_y] == 1)
+			{
+				que.x[++que.rear] = next_x;
+				que.y[que.rear] = next_y;
+
+				adj[next_x][next_y] = /*adj[x][y] + 1*/num;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	
+	int adj[26][26] = { 0, };
+	int visited[26] = { 0, };
+
+	int i, j, k;
+
+	int num = 2;
+
+	int cnt[625] = { 0, };
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			scanf("%1d", &adj[i][j]);
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			if (adj[i][j] == 1)
+			{
+				BFS(adj, n, i, j, num);
+				num++;
+			}
+		}
+	}
+
+	printf("%d", num - 2);
+
+	for (k = 2; k < num; k++)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (adj[i][j] == k)
+				{
+					cnt[k]++;
+				}
+			}
+		}
+	}
+
+	int temp;
+
+	for (i = 0; i < num-1; i++)
+	{
+		for (j = i+1; j < num; j++)
+		{
+			if (cnt[i] > cnt[j])
+			{
+				temp = cnt[i];
+				cnt[i] = cnt[j];
+				cnt[j] = temp;
+			}
+		}
+	}
+
+	for (i = 2; i < num; i++)
+	{
+		printf("\n%d", cnt[i]);
+	}
+
+	return 0;
 }
